a1571762095324258d19431c43882d155e89310d
francisco-filho <francisco@capimgrosso.com>
2017-10-20 20:35:39 -0200

    Atualizando react 16

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class App extends Component {
-  render(){
-    return <div>
-      <h1>React Hot Loader Boilerplate!</h1>
-    </div>
-  }
+export default function App() {
+  return <div>
+    <h1>Hello Worlda</h1>
+  </div>
 }
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,7 +1,16 @@
-import { expect } from 'chai'
+import React from 'react'
+import './enzyme-config'
+import App from '../src/App'
+import {shallow, mount} from 'enzyme'
 
-describe('Test Example', () => {
-  it('Should pass', () => {
-    expect(true).to.be.true
+describe('App', () => {
+  it('Deve estar definido', () => {
+    const subject = shallow(<App/>)
+    expect(subject.contains("Hello World")).toBeTruthy()
+  })
+
+  it('Deve funcionar com mount()', () => {
+    const subject = mount(<App/>)
+    expect(subject.contains("Hello World")).toBeTruthy()
   })
 })
